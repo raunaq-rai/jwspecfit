08c3ce561b14035ca3d667d019c4fde1ce15d83e
Raunaq <raunaq.rai@hotmail.co.uk>
2026-05-18 16:06:38 +0100

    plotting: relabel HEI_7067 as 'HeI 7065' (air-wavelength convention)

--- src/jwspecfit/plotting.py
+++ src/jwspecfit/plotting.py
@@ -767,7 +767,7 @@ def plot_spectrum_interactive(
             "Ha": "Hα", "NII_6585": "[NII] 6585",
             "HEI_6680": "HeI 6680",
             "SII_6718": "[SII]6716", "SII_6732": "[SII]6731",
-            "HEI_7067": "HeI 7067",
+            "HEI_7067": "HeI 7065",
             "ArIII_7138": "[ArIII] 7138",
         }
 
